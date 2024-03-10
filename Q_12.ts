a76e12f20614c9ee50c919654fa074a6b4b73e01
Zulfiqar Ali <156158595+Zulfiqar67573@users.noreply.github.com>
2024-03-10 05:14:33 -0700

    Add files via upload

---
+++ Q_12.ts
@@ -0,0 +1,8 @@
+// Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+// should be the same, but each message should be personalized with the person’s name.
+
+let personName:string[]=["Zulfiqar","hassan","Ahmed","Naseem"]
+console.log( `Hi ${personName[0]} , Are you ready for coding?  `);
+console.log( `Hi ${personName[1]} , Are you ready for coding?  `);
+console.log( `Hi ${personName[2]} , Are you ready for coding?  `);
+console.log( `Hi ${personName[3]} , Are you ready for coding?  `);
