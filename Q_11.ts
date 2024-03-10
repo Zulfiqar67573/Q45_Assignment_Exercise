82ebd7b0d4b7d805cc4fb7390a727098f0ede6c2
Zulfiqar Ali <156158595+Zulfiqar67573@users.noreply.github.com>
2024-03-10 04:44:38 -0700

    Add files via upload

---
+++ Q_11.ts
@@ -0,0 +1,11 @@
+// 11.Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+
+let zulfiqarAbbas:string[]=["Zulfiqar","Najif","Ali","Asad" ,"Attahussain"]
+//      index arrays       [0]      [1]    [2]   [3]       [4]
+console.log(zulfiqarAbbas[0])
+console.log(zulfiqarAbbas[1])
+console.log(zulfiqarAbbas[2])
+console.log(zulfiqarAbbas[3])
+console.log(zulfiqarAbbas[4])
