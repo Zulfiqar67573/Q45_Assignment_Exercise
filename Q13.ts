75b2aeb60f1a354bdc656f066e7429b47656447e
Zulfiqar Ali <156158595+Zulfiqar67573@users.noreply.github.com>
2024-04-06 21:36:30 +0500

    Add files via upload

---
+++ Q13.ts
@@ -0,0 +1,4 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+//to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let transportation:string[]=["honda car","motorcycle","cultus car","bike"]
+transportation.map((items)=>console.log(`"i would like to own a ${items}."`))
