18a5c75c6a351a943cb8e02bfe3669c72a972bc6
Zulfiqar Ali <156158595+Zulfiqar67573@users.noreply.github.com>
2024-04-06 21:50:37 +0500

    Add files via upload

---
+++ Q_14.ts
@@ -0,0 +1,6 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let GuestsList:string[]=["Imtiaz","Zulfiqar","Atta Hussain"]
+GuestsList.map((items)=>
+console.log(`Dear ${items}, "I would like to invite you for a dinner."`))
+
